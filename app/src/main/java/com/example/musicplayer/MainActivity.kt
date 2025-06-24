97e99d9fea9cc787cb8e680d77822558fb6f7a16
buildscript-dev <buildscript.dev@gmail.com>
2025-06-24 17:57:18 +0530

    Button Rec and Cir is Added

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.musicplayer
 
+import CirBtn
+import RecBtn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.musicplayer.ui.theme.MusicPlayerTheme
+import com.example.musicplayer.ui.theme.PrimaryColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +27,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MusicPlayerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+Homepage()
             }
         }
+        }
     }
-}
+
+
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Homepage(){
+    Column(modifier = Modifier.fillMaxSize()
+        .background(color = PrimaryColor),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    { RecBtn(icon = Icons.Default.Home, onClick  = {})
+       Spacer(Modifier.height(100.dp))
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MusicPlayerTheme {
-        Greeting("Android")
+        CirBtn(icon = Icons.Default.Home, onClick  = {})
     }
 }
---
+++ app/src/main/java/com/example/musicplayer/component/VoidBtn.kt
@@ -0,0 +1,79 @@
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import com.example.musicplayer.ui.theme.PrimaryColor
+
+@Composable
+fun RecBtn(
+    onClick: () -> Unit,
+    icon: ImageVector? = null,
+) {
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(12.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = PrimaryColor,
+            contentColor = Color.White
+        ),
+        contentPadding = PaddingValues(0.dp),
+        elevation = ButtonDefaults.buttonElevation(defaultElevation = 0.dp),
+        modifier = Modifier
+            .width(70.dp)
+            .height(45.dp)
+            .border(width = 4.dp, color = Color.Black, shape = RoundedCornerShape(12.dp))
+    ) {
+        icon?.let {
+            Icon(
+                imageVector = it,
+                contentDescription = "Back",
+                tint = Color.White
+            )
+        }
+    }
+}
+
+
+
+@Composable
+fun CirBtn(
+    onClick: () -> Unit,
+    icon: ImageVector? = null,
+) {
+    Button(
+        onClick = onClick,
+        shape = CircleShape,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = PrimaryColor,
+            contentColor = Color.White
+        ),
+        contentPadding = PaddingValues(0.dp),
+        elevation = ButtonDefaults.buttonElevation(defaultElevation = 0.dp),
+        modifier = Modifier.size(70.dp)
+            .border(width = 4.dp, color = Color.Black, shape = CircleShape)
+    ) {
+        icon?.let {
+            Icon(
+                imageVector = it,
+                contentDescription = "Back",
+                tint = Color.White
+            )
+        }
+    }
+}
